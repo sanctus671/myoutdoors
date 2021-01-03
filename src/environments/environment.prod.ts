15313ff3de71b97a240430b74ab1f3ecd6c294ac
sanctus671 <taylor@taylorhamling.com>
2021-01-04 12:18:34 +1300

    initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+    production: true,
+    apiUrl: 'https://www.getoutdoors.co.nz/api/public/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+    production: false,
+    apiUrl: 'https://www.my-outdoors.co.nz/api/public/api'
 };
 
 /*
